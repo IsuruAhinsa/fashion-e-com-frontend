a02082b84cd56e2227e93729cf17618a7a1758b2
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-02-04 18:32:19 +0530

    Fixed Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -48,22 +48,22 @@ const Pagination = ({
 
   return (
     <nav className="border-t border-gray-200 px-4 my-8 flex items-center justify-between sm:px-0">
-      
       <div className="-mt-px w-0 flex-1 flex">
-        <div
-          onClick={paginatePrevious}
-          className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:cursor-pointer"
-        >
-          <ArrowLongLeftIcon
-            className="mr-3 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          Previous
-        </div>
+        {currentPage !== 1 && (
+          <div
+            onClick={paginatePrevious}
+            className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:cursor-pointer"
+          >
+            <ArrowLongLeftIcon
+              className="mr-3 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+            Previous
+          </div>
+        )}
       </div>
 
       <div className="hidden md:-mt-px md:flex">
-
         {pageNumbers.map((number) => {
           if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
             return (
@@ -78,22 +78,22 @@ const Pagination = ({
             );
           }
         })}
-
       </div>
 
       <div className="-mt-px w-0 flex-1 flex justify-end">
-        <div
-          onClick={paginateNext}
-          className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:cursor-pointer"
-        >
-          Next
-          <ArrowLongRightIcon
-            className="ml-3 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </div>
+        {currentPage < pageNumbers.length  && (
+          <div
+            onClick={paginateNext}
+            className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:cursor-pointer"
+          >
+            Next
+            <ArrowLongRightIcon
+              className="ml-3 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </div>
+        )}
       </div>
-      
     </nav>
   );
 };
